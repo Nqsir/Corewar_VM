e828df80255e061f68f993797bef4bb1caa41c30
fpupier <fpupier@student.le-101.fr>
2018-12-13 08:48:24 +0100

    test / debug opcode

--- vm_champs/champs/examples/zork.s
+++ vm_champs/champs/examples/zork.s
@@ -5,8 +5,8 @@
 
 bite:	sti r1, %:live, %1
         ld %-254, r2
-        ld 4, r3
-        and r2, r3, r4
+        #ld 525, r3
+        and -1, -1, r4
         #or r2, r3, r4
         #ldi r3, %2, r4
         sti r4, %20, %0
