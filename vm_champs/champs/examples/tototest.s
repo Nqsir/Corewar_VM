e3418ef3909cd3b3402a7928b3194e3259bf902a
Nqsir <steyaert.victor@gmail.com>
2019-10-25 14:37:45 +0200

    [Clean]

--- vm_champs/champs/examples/tototest.s
+++
@@ -1,9 +0,0 @@
-.name "tototest"
-.comment "ICH BIN TOTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
-
-bite:	ld %78119169, r2
-		ld %16777216, r3
-		sti r2, %14, %0
-		sti r3, %11, %0
-		and %1, %1, r1
-
